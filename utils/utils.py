2734def516dfec053e8a144912670ae8e2afef77
LPT-FA3163\Altran <aleixo.da@gmail.com>
2017-04-26 19:39:05 +0100

    [UPDATE] deleteDuplicateImages script to show plots and rebuild algorithm

--- utils/utils.py
+++ utils/utils.py
@@ -29,114 +29,127 @@ utilMethods.renameFilesInFolder("t","forrest")
 """
 
 class Utils(object):
+    
+    colors = ('b', 'g', 'r')
+    def shuffle_in_unison(a, b):
+        assert len(a) == len(b)
+        shuffled_a = np.empty(a.shape, dtype=a.dtype)
+        shuffled_b = np.empty(b.shape, dtype=b.dtype)
+        permutation = np.random.permutation(len(a))
+        
+        for old_index, new_index in enumerate(permutation):
+            
+            shuffled_a[new_index] = a[old_index]
+            shuffled_b[new_index] = b[old_index]
+            
+        return shuffled_a, shuffled_b
+    
+    def deleteDuplicatedImagesInDir(self,dir):
+        
+        colors = ('b', 'g', 'r')
+        hists = []
+        hists2 = []
+        indexInComparison = 0
+        duplicate = 0
+        initialFilesLen = len(os.listdir(dir))
+        print("[UTILS] Directory has {} ".format(initialFilesLen))
+        
+        for i,imagePath in enumerate(paths.list_images(dir)):
+            
+            hists = []  
+            image = cv2.imread(imagePath)
+            channels = cv2.split(image)
+            indexInComparison = i
+            
+            for (channel,color) in zip(channels,colors):
+            
+                hists.append(cv2.calcHist([channel],[0],None,[256],[0,256]))
+                
+               
+            for i2,imagePath2 in enumerate(paths.list_images(dir)):
+                hists2 = []
+                if i2 > indexInComparison:
+                
+                    print("[UTILS] Checking for duplicate images in path {} and {}".format(imagePath,imagePath2))
+                    
+                    image2 = cv2.imread(imagePath2)
+                    channels2 = cv2.split(image2)
+                    
+                    self.plot3DHistogram(image,image2,dir)
+     
+                    for (channel2,color) in zip(channels2,colors):
+                        
+                        hists2.append(cv2.calcHist([channel2],[0],None,[256],[0,256]))
 
-	colors = ('b', 'g', 'r')
+                    duplicate = 0
+                    for (h1,h2) in zip(hists,hists2):
+                        
+                        if len(h1) is 256 and len(h2) is 256:
+                            
+                            if cv2.compareHist(h1,h2,cv2.cv.CV_COMP_BHATTACHARYYA) == 0.0:
+                                
+                                duplicate +=1
+                                
+                                if duplicate == 3:
+                                    print("[DELETE DUPLICATE IMAGES] Equal images {} {}".format(imagePath,imagePath2))
+                                    os.remove(imagePath2)
+                                    
+            
+            print("[UTILS] Folder had {} images and now has {}. {} were deleted".
+                  format(initialFilesLen,
+                         len(os.listdir(dir)),
+                         initialFilesLen - len(os.listdir(dir))))
+                    
+                                    
+                                    
+    def plotImage3DHistogram(self,image,dir):
+        
+        print("[UTILS] Plot 3D histogram")
+        for i,color in enumerate(self.colors):
 
-	def resizeImages(path):
-		print(path)
-		directory = path
-		try:
-			for imagef in os.listdir(directory):
-				if imagef == ".DS_Store":
-					pass
-				else:
-					img = os.path.join(directory,imagef)
-					image = cv2.resize(cv2.imread(img),(32,32),interpolation = cv2.INTER_AREA)
-					cv2.imwrite(img,image)
-		except Exception as e:
-			print("ERROR {}".format(e))
-		finally:t
-		print("[INFO] Images with 32*32")
-	def changeImgNames():
-		directory = "/Users/diogoaleixo/Desktop/Mestrado/images_raw/images/"
-		try:
-			for i,imagef in enumerate(os.listdir("/Users/diogoaleixo/Desktop/Mestrado/images_raw/images/")):
-				if imagef == ".DS_Store":
-					pass
-				else:
-					img = os.path.join(directory,imagef)
-					if imagef.split('_')[0] == "fire":
-						os.rename(img,"fire_"+str(i)+".jpg")
-					else:
-						os.rename(img,"forrest_"+str(i)+".jpg")
-		except Exception as e:
-			print("ERROR {}".format(e))
-		finally:
-			print("[INFO] Image names changed")
+            hist = cv2.calcHist([image], [1], None, [256], [0, 256])
+            ax1 = plt.subplot(211)
+            ax1 = plt.plot(hist, color=color)
+            ax1 = plt.xlim([0, 256])
 
+        #plt.show()
+        #pltlab.savefig(dir+str(random.uniform(0.0,10000.0))+".png")
 
-	def shuffle_in_unison(a, b):
+    def plot3DHistogram(self,image1,image2,saveDir):
 
-		assert len(a) == len(b)
-		shuffled_a = np.empty(a.shape, dtype=a.dtype)
-		shuffled_b = np.empty(b.shape, dtype=b.dtype)
-		permutation = np.random.permutation(len(a))
-		for old_index, new_index in enumerate(permutation):
-			shuffled_a[new_index] = a[old_index]
-			shuffled_b[new_index] = b[old_index]
-		return shuffled_a, shuffled_b
+        for i,color in enumerate(self.colors):
 
-	def deleteDuplicated(self,destination_dir):	
+            hist = cv2.calcHist([image1], [i], None, [256], [0, 256])
+            ax1 = plt.subplot(221)
+            ax1 = plt.plot(hist, color=color)
+            ax1 = plt.xlim([0, 256])
+            ax1 = plt.ylabel("Histogram in comparision")
+            
+            
+        for i,color in enumerate(self.colors):
+            
+            ax1 = plt.subplot(222)
+            ax1 = plt.imshow(cv2.cvtColor(image1, cv2.COLOR_BGR2RGB))
+            ax1 = plt.ylabel("Image in comparision")
+        
+        for i,color in enumerate(self.colors):
 
-		colors = ('b','g','r')
-		hists = []
-		hists2 = []
-		indexInComparison = 0
-		duplicate = 0	
-		for i,imagePath in enumerate(paths.list_images(destination_dir)):		
-			image = cv2.imread(imagePath)
-			channels = cv2.split(image)
-			indexInComparison = i
-		
-			for (channel,color) in zip(channels,colors):
+            hist = cv2.calcHist([image2], [i], None, [256], [0, 256])
+            ax1 = plt.subplot(223)
+            ax1 = plt.plot(hist, color=color)
+            ax1 = plt.xlim([0, 256])
+            ax1 = plt.ylabel("Histogram to compara")
+            
+        for i,color in enumerate(self.colors):
 
-				hist = cv2.calcHist([channel],[0],None,[256],[0,256])	
-				hists.append(hist)
-		
-			for i,imagePath2 in enumerate(paths.list_images(destination_dir)):
-				if i > indexInComparison:				
-					image2 = cv2.imread(imagePath2)
-					channels2 = cv2.split(image2)
-					for (channel2,color2) in zip(channels2,colors):
-						hist2 = cv2.calcHist([channel2],[0],None,[256],[0,256])	
-						hists2.append(hist2)					
-					for (h1,h2) in zip(hists,hists2):					
-						res = cv2.compareHist(h1,h2,cv2.cv.CV_COMP_BHATTACHARYYA)	
-						if res == 0.0:
-							duplicate += 1									
-					if duplicate == 3:
-						print("[INFO] IGUAIS {} {}".format(imagePath,imagePath2))
-						print("[INFO] Eliminar {}".format(imagePath2))
-						os.remove(imagePath2)										
-					duplicate = 0
-					hists2 = []
-			hists = []
+            ax1 = plt.subplot(224)
+            ax1 = plt.imshow(cv2.cvtColor(image2, cv2.COLOR_BGR2RGB))
+            ax1 = plt.ylabel("Image to compare")
+            
+        plt.show()
+        #pltlab.savefig(saveDir+str(random.uniform(0.0,10000.0))+".png")
 
-	def plot3DHistogram(self,image1,image2,saveDIr):
-
-		for i,color in enumerate(self.colors):
-
-			hist = cv2.calcHist([image1], [i], None, [256], [0, 256])
-			ax1 = plt.subplot(211)
-			ax1 = plt.plot(hist, color=color)
-			ax1 = plt.xlim([0, 256])
-
-
-		for i,color in enumerate (self.colors):
-
-			hist = cv2.calcHist([image2], [i], None, [256], [0, 256])
-			ax2 = plt.subplot(212)
-			ax2 = plt.plot(hist, color=color)
-			ax2 = plt.xlim([0, 256])
-
-		#plt.show()
-		pltlab.savefig("Histograms/hist_"+str(random.uniform(0.0,10000.0))+".png")
-
-	def deleteDebugImages(self):
-		for filename in glob.glob("Histograms/hist_*"):
-			os.remove(filename)
-
-	def renameFilesInFolder(self,destination_dir,prefix):
-  		for filename in os.listdir(destination_dir):
-  			if not filename.startswith("."):
-  				os.rename(destination_dir + filename,destination_dir + prefix+"-"+str(random.uniform(0.0,10000.0))+".png")
+    def renameFilesInFolder(self,destination_dir,prefix):
+        for filename in os.listdir(destination_dir):
+            if not filename.startswith("."):
+                os.rename(destination_dir + filename,destination_dir + prefix+"-"+str(random.uniform(0.0,10000.0))+".png")
